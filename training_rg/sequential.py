9cf4292e6d31f1b7f35d826f175ed7bea32d6763
Alexis Torres Valdes <alexis89.dev@gmail.com>
2022-01-12 13:36:24 -0500

    Pasar las rutas de directorios de rutinas por un archivo

--- training_rg/sequential.py
+++ training_rg/sequential.py
@@ -18,12 +18,12 @@ from training_rg.constants import WEEKDAYS, SESSIONS_COUNT, SESSIONS_DAYS_OFF, S
 from training_rg.logger import logger
 
 
-def sequence(dir_routines, count=30, first_day=WeekDay.MONDAY.value, days_off=1, step=0):
+def sequence(routines, count=30, first_day=WeekDay.MONDAY.value, days_off=1, step=0):
     """
     La idea es dada una lista, ordenada, de directorios con rutinas,
     generar cierta cantidad de sesiones, de entrenamiento, con determinados
     dias de descanso y a ciertos intervalos
-    :param dir_routines: Lista de diretorios con las rutinas. El orden importa.
+    :param routines: Lista de listas de rutinas de entrenamiento
     :param count: Cantidad de sesiones
     :param first_day: Primer dia de la semana a considerar
     :param days_off: Dias de descanso
@@ -31,13 +31,9 @@ def sequence(dir_routines, count=30, first_day=WeekDay.MONDAY.value, days_off=1,
     :return: list de sesiones
     """
     if step < 1:
-        step = len(dir_routines)
+        step = len(routines)
 
     sessions = []
-
-    dir_routines_files = []
-    for root in dir_routines:
-        dir_routines_files.append(_walk(root))
 
     r = Random()
 
@@ -45,7 +41,7 @@ def sequence(dir_routines, count=30, first_day=WeekDay.MONDAY.value, days_off=1,
     for b in range(0, count, step + days_off):
         for day_i in range(b, b + step):
             day = WEEKDAYS[(first_day + day_i) % len(WEEKDAYS)]
-            full_path = r.choice(dir_routines_files[j % len(dir_routines_files)])
+            full_path = r.choice(routines[j % len(routines)])
             head, tail = os.path.split(full_path)
             sessions.append({'day': day, 'full_path': full_path, 'name': tail})
             j += 1
@@ -67,10 +63,9 @@ def parse_args():
                         version='%(prog)s was created by software developer Alexis Torres Valdes <alexisdevsol@gmail.com>',
                         help="Show program's author and exit")
 
-    parser.add_argument('--dir-routines',
-                        nargs='+',
+    parser.add_argument('--routines',
                         required=True,
-                        help='Routine directories. Order is important')
+                        help='File with the paths to the routines directories')
     parser.add_argument('--dir-output',
                         required=True,
                         help='Directory containing programmed routines')
@@ -112,7 +107,7 @@ def parse_args():
 def main():
     try:
         args = parse_args()
-        arg_dir_routines = args.dir_routines
+        arg_routines = args.routines
         arg_dir_output = args.dir_output
         arg_first_day = args.first_day
         arg_count = args.count
@@ -121,12 +116,8 @@ def main():
         arg_yes = args.yes
         arg_verbose = args.verbose
 
-        # arg_dir_routines = [
-        #     '/media/dev/cbc809ad-b091-4a86-9ac1-410e18469ead/media/entrenamiento/[Ejercicios] Fausto Murillo/1.fullbody/',
-        #     '/media/dev/cbc809ad-b091-4a86-9ac1-410e18469ead/media/entrenamiento/[Ejercicios] Fausto Murillo/2.gap/',
-        #     '/media/dev/cbc809ad-b091-4a86-9ac1-410e18469ead/media/entrenamiento/[Ejercicios] Fausto Murillo/3.abdominales/'
-        # ]
-        # arg_dir_output = '/media/dev/cbc809ad-b091-4a86-9ac1-410e18469ead/media/entrenamiento/Mis rutinas/dev/'
+        # arg_routines = 'routines.txt'
+        # arg_dir_output = '/media/dev/cbc809ad-b091-4a86-9ac1-410e18469ead/media/entrenamiento/Mis rutinas/dev1/'
         # arg_first_day = 'Sunday'
         # arg_count = 30
         # arg_step = 3
@@ -139,9 +130,20 @@ def main():
             ch.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
             logger.addHandler(ch)
 
-        for path in arg_dir_routines:
-            if not os.path.exists(path):
-                raise ValueError(f"'{path}' directory does not exist")
+        routines = []
+        if not os.path.exists(arg_routines):
+            raise ValueError('The file with the routine directory paths does not exist.')
+
+        with open(arg_routines) as fr:
+            for line in fr:
+                path = line.strip('\n')
+                if not os.path.exists(path):
+                    raise ValueError(f"'{path}' directory does not exist")
+                path_walk = _walk(path)
+                routines.append(path_walk)
+
+        if not len(routines):
+            raise ValueError('At least one routine directory path is required.')
 
         if not os.path.exists(arg_dir_output):
             raise ValueError(f"'{arg_dir_output}' directory does not exist")
@@ -156,7 +158,7 @@ def main():
             raise ValueError('At least 1 day without training is necessary')
 
         first_day = WEEKDAYS.index(arg_first_day)
-        sessions = sequence(arg_dir_routines,
+        sessions = sequence(routines,
                             count=arg_count,
                             first_day=first_day,
                             days_off=arg_days_off,
@@ -178,11 +180,12 @@ def main():
             _copy(sessions, arg_dir_output, lambda file_name_dst: print(file_name_dst))
             print('\nRoutines successfully copied!')
         elif resp == KeyResp.NO.value:
-            sys.exit()
+            sys.exit(os.EX_SOFTWARE)
         else:
             raise ValueError(f'Only answers ({KeyResp.YES.value}/{KeyResp.NO.value}) are allowed')
     except Exception as err:
         logger.error(err)
+        print(err)
         sys.exit(os.EX_SOFTWARE)
 
 
